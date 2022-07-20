63b5cb93382f69252c5ef99f2187669e3919ea11
andrew-retherford <andrewretherford@gmail.com>
2022-07-20 10:49:54 -0700

    Added better error handling to Cast and Crew

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,44 +1,60 @@
 import CastMember from '../CastMember/CastMember';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap'
+import { apiResultReducer } from '../../functions/apiResultReducer';
 
 const Cast = () => {
-    const [cast, setCast] = useState(null)
+    const initialState = {
+        loading: false,
+        result: null,
+        error: ''
+    }
+
+    const [apiState, dispatch] = useReducer(apiResultReducer, initialState)
     const { showId } = useParams()
+    const { result, loading, error } = apiState
 
     useEffect(() => {
+        dispatch({ type: 'loading' })
         fetch(`https://api.tvmaze.com/shows/${showId}/cast`)
-            .then(res => res.json())
-            .then(data => {
-                setCast(data)
+        .then(res => {
+            if(res.status === 404) {
+                return dispatch({
+                    type: 'error',
+                    error: `Oops, couldn't find any cast for this show!`
+                })
+            } else if(res.status === 200 || res.status === 304) {
+                return res.json()
+            }
+        })
+        .then(data => {
+            dispatch({
+                type: 'success',
+                data: data
             })
+        })
+        .catch(err => {
+            dispatch({
+                type: 'error',
+                error: 'Oops, something went wrong! Please try again later.'
+            })
+            console.log(err)
+        })
     },[showId])
 
-    if(!cast) {
-        return (
-            <Container className='d-flex justify-content-center'>
-                <h1>Loading...</h1>
-            </Container>
-        )
-    } else if(cast.length < 1) {
-        return (
-            <Container className='d-flex justify-content-center'>
-                <h1>No Results</h1>
-            </Container>
-        )
-    } else {
-        return (
-            <Container className='mt-4 mb-4'>
+    return (
+        <Container className='mt-4 mb-4'>
+            {result &&
                 <Row xs={1} md={2} lg={3} xl={4} className='g-4'>
-                    {cast ?
-                        cast.map((castMember, index) => <CastMember key={index} castMemberInfo={castMember}/>)
-                        : <p>Loading...</p>
-                    }    
+                    {result.map((castMember, index) => <CastMember key={index} castMemberInfo={castMember}/>)}
                 </Row>
-            </Container>
-        );
-    }
+            }
+            {loading && <Container className='d-flex justify-content-center'><h1>Loading...</h1></Container>}
+            {result && result.length < 1 && <Container className='d-flex justify-content-center'><h1>No Results</h1></Container>}
+            {error && <Container className='d-flex justify-content-center'><h1>{error}</h1></Container>}
+        </Container>
+    )
 };
 
 export default Cast;
--- src/components/Crew/Crew.js
+++ src/components/Crew/Crew.js
@@ -1,45 +1,61 @@
 import CrewMember from '../CrewMember/CrewMember';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap'
 import { removeDuplicates } from '../../functions/removeDuplicates';
+import { apiResultReducer } from '../../functions/apiResultReducer';
 
 const Crew = () => {
-    const [crew, setCrew] = useState(null)
+    const initialState = {
+        loading: false,
+        result: null,
+        error: ''
+    }
+
+    const [apiState, dispatch] = useReducer(apiResultReducer, initialState)
     const { showId } = useParams()
+    const { result, loading, error } = apiState
 
     useEffect(() => {
+        dispatch({ type: 'loading' })
         fetch(`https://api.tvmaze.com/shows/${showId}/crew`)
-            .then(res => res.json())
-            .then(data => {
-                setCrew(removeDuplicates(data))
+        .then(res => {
+            if(res.status === 404) {
+                return dispatch({
+                    type: 'error',
+                    error: `Oops, couldn't find any crew for this show!`
+                })
+            } else if(res.status === 200 || res.status === 304) {
+                return res.json()
+            }
+        })
+        .then(data => {
+            dispatch({
+                type: 'success',
+                data: removeDuplicates(data)
             })
+        })
+        .catch(err => {
+            dispatch({
+                type: 'error',
+                error: 'Oops, something went wrong! Please try again later.'
+            })
+            console.log(err)
+        })
     },[showId])
 
-    if(!crew) {
-        return (
-            <Container className='d-flex justify-content-center'>
-                <h1>Loading...</h1>
-            </Container>
-        )
-    } else if(crew.length < 1) {
-        return (
-            <Container className='d-flex justify-content-center'>
-                <h1>No Results</h1>
-            </Container>
-        )
-    } else {
-        return (
-            <Container className='mt-4 mb-4'>
+    return (
+        <Container className='mt-4 mb-4'>
+            {result &&
                 <Row xs={1} md={2} lg={3} xl={4} className='g-4'>
-                    {crew ?
-                        crew.map((crewMember, index) => <CrewMember key={index} crewMemberInfo={crewMember}/>)
-                        : <p>Loading...</p>
-                    }    
+                    {result.map((crewMember, index) => <CrewMember key={index} crewMemberInfo={crewMember}/>)}    
                 </Row>
-            </Container>
-        );
-    }
+            }
+            {loading && <Container className='d-flex justify-content-center'><h1>Loading...</h1></Container>}
+            {result && result.length < 1 && <Container className='d-flex justify-content-center'><h1>No Results</h1></Container>}
+            {error && <Container className='d-flex justify-content-center'><h1>{error}</h1></Container>}
+        </Container>
+    );
 };
 
 export default Crew;
